20d143b7c40cafdd2c5abdd63e0012603f6dabad
Sanny <krustapie@outlook.com>
2025-02-19 18:55:08 +0000

    Add ASD

---
+++ projects/platformer/platformer.js
@@ -0,0 +1,86 @@
+$(function () {
+  // initialize canvas and context when able to
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  window.addEventListener("load", loadJson);
+
+  function setup() {
+    if (firstTimeSetup) {
+      halleImage = document.getElementById("player");
+      projectileImage = document.getElementById("projectile");
+      cannonImage = document.getElementById("cannon");
+      $(document).on("keydown", handleKeyDown);
+      $(document).on("keyup", handleKeyUp);
+      firstTimeSetup = false;
+      //start game
+      setInterval(main, 1000 / frameRate);
+    }
+    // Create walls - do not delete or modify this code
+    createPlatform(-50, -50, canvas.width + 100, 50); //top
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200); //right
+    createPlatform(-50, -50, 50, canvas.height + 500); //bottom
+    createPlatform(canvas.width, -50, 50, canvas.height + 100);
+
+    /**
+     * Uncomment the drawGrid() function call below to add a "grid" to your platformer game's screen
+     * The grid will place both horizontal and vertical platforms incremented 100 pixels apart
+     * This can help you determine specific x any y values throughout the game
+     * Comment the function call out to remove the grid
+     */
+
+    drawGrid();
+
+    /////////////////////////////////////////////////
+    //////////ONLY CHANGE BELOW THIS POINT///////////
+    /////////////////////////////////////////////////
+
+    // TODO 1
+    // Create platforms
+    // You must decide the x position, y position, width, and height of the platforms
+    // example usage: createPlatform(x,y,width,height)
+    createPlatform(200,700,100,100)
+    createPlatform(300,600,100,100)
+    createPlatform(400,500,100,100)
+    createPlatform(500,400,100,100)
+    createPlatform(600,300,100,100)
+    createPlatform(700,200,100,100)
+
+    createPlatform(800,120,100,100)
+
+    createPlatform(900,120,300,7)
+    createPlatform(900,120,300,7)
+    createPlatform(900,120,300,7)
+    
+
+
+    
+    // TODO 2
+    // Create collectables
+    // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
+    // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
+    // example usage: createCollectable(type, x, y, gravity, bounce)
+    createCollectable('diamond', 130, 600, 1, 1)
+    createCollectable('max', 230, 500, 1, 1)
+    createCollectable('steve', 330, 400, 1, 1)
+    createCollectable('grace', 430, 300, 1, 1)
+// TODO 3
+    // Create cannons
+    // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
+    // Your wall choices are: 'top' 'left' 'right' and 'bottom'
+    // example usage: createCannon(side, position, delay, width, height)
+    createCannon('bottom', 300, 1050, 50, 50)
+    createCannon('right', 300, 1000, 50, 50)
+
+    createCannon('left', 400-(118/2), 1000, 50, 50)//centering on left
+    createCannon('top', 250+(118/2), 3000, 75, 500)//Centering on top
+    //Width of 118
+M
+
+    
+    /////////////////////////////////////////////////
+    //////////ONLY CHANGE ABOVE THIS POINT///////////
+    /////////////////////////////////////////////////
+  }
+
+  registerSetup(setup);
+});
