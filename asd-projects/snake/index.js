d0c2855bd69a98772887b8a3af9f1f7ef3a9d2f7
Sanny <krustapie@outlook.com>
2025-03-10 18:23:03 +0000

    Started Snake

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -11,6 +11,9 @@ var highScoreElement = $("#highScore");
 
 // TODO 4a: Create the snake, apple and score variables
 // Game Variables
+var snake = {};
+var apple = {};
+var score = 0;
 
 // Constant Variables
 var ROWS = 20;
@@ -43,9 +46,8 @@ function init() {
   // TODO 4c-2: initialize the snake
 
   // TODO 4b-2: initialize the apple
-
+  makeApple();
   // TODO 5a: Initialize the interval
-
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -184,7 +186,18 @@ function endGame() {
  * position on the board that is not occupied and position the apple there.
  */
 function makeApple() {
-  // TODO 4b-1: Fill in the makeApple() code block
+  // make the apple jQuery Object and append it to the board
+  apple.element = $("<div>").addClass("apple").appendTo(board);
+
+  // get a random available row/column on the board
+  var randomPosition = getRandomAvailablePosition();
+
+  // initialize the row/column properties on the Apple Object
+  apple.row = randomPosition.row;
+  apple.column = randomPosition.column;
+
+  // position the apple on the screen
+  repositionSquare(apple);
 }
 
 /* Create an HTML element for a snakeSquare using jQuery. Then, given a row and
@@ -208,7 +221,6 @@ function makeSnakeSquare(row, column) {
 */
 function handleKeyDown(event) {
   // TODO 6a: make the handleKeyDown function register which key is pressed
-  
 }
 
 /* Given a gameSquare (which may be a snakeSquare or the apple), position
